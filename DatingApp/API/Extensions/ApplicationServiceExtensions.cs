a1c18382367b0cc3d4a701d9b0313c5fe0055de6
wiktord2000 <wiktord2000@wp.pl>
2022-08-17 22:51:48 +0200

    Section 11: Adding photo upload functionality (1/3)

--- DatingApp/API/Extensions/ApplicationServiceExtensions.cs
+++ DatingApp/API/Extensions/ApplicationServiceExtensions.cs
@@ -17,11 +17,17 @@ namespace API.Extensions
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config){
 
+            // Configure Cloudinary - using private data in appsettings.json
+            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
+
             // We could use services.AddSingleton but this service will be present until the app stop - we don't need it
             // We could use services.Trasnsient - service expire every time when any method is executed (also not need it)
             // We use services.addScoped - service is creating only to work with particular http requst
             services.AddScoped<ITokenService, TokenService>();    // we could only create class TokenService but for the testing purpose we should create also interface
             
+            // Add Clodinary service
+            services.AddScoped<IPhotoService, PhotoService>();
+
             // Add repository
             services.AddScoped<IUserRepository, UserRepository>();
 
